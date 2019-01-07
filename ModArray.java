6dbe0dc93f2b65ed6f6291137018d6c83a246a81
7efm <38299622+7efm@users.noreply.github.com>
2019-01-07 13:02:01 -0800

    Initial commit

---
+++ ModArray.java
@@ -0,0 +1,16 @@
+
+/**
+ * Write a description of class ModArray here.
+ *
+ * @author Jacinto Garcia
+ * @version 1.7.19
+ */
+public class ModArray
+{
+    public void add(int num, int index){
+    
+    }
+    public void remove(int index){
+        
+    }
+}
