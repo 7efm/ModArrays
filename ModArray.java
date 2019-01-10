08768e07ffc14e532d023c3008230d73a4b8a63c
7efm <38299622+7efm@users.noreply.github.com>
2019-01-10 13:01:20 -0800

    Finished remove + started add

--- ModArray.java
+++ ModArray.java
@@ -7,28 +7,31 @@
  */
 public class ModArray
 {
-    int[] array = {4,7,8,9,3,5,2};
+    int[] array = {4,7,8,9,3,5,4};
     public void add(int num, int index){
-
+        System.out.println(index);
+        System.out.println(array.length-1);
+        for(int i = array.length-1; i > index; i--){
+            System.out.println(i);
+            array[i] = array[i-1];
+        }
+        printArray(); 
     }
 
     public void remove(int index){
+        printArray(); 
+        for(int i = index; i <= array.length-2; i++){
+            array[i] = array[i+1];
+        }
+        array[array.length-1] = 0;
+        printArray();
+    }
+
+    public void printArray(){
         for(int i = 0; i < array.length; i++){
             System.out.print(array[i] + " ");
 
         }
         System.out.println();
-        System.out.println(array.length-1);
-        for(int i = index; i < array.length-1; i++){
-
-            if(i != array.length-1){
-                array[i] = array[i+1];
-            }
-        }
-        array[array.length-1] = 0;
-        for(int i = 0; i < array.length; i++){
-            System.out.print(array[i] + " ");
-
-        }
     }
 }
